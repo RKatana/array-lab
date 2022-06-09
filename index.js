332b83028bffb7c5abf943054ebe8a1ed5ecbfb5
RKatana <roduor41@gmail.com>
2022-06-09 18:43:30 +0300

    fixed all the tests

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+// const functionToString = require("sinon/lib/sinon/util/core/function-to-string")
+
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    return [...cats, name]
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeFirstCat(){
+    // slice
+    console.log(cats.slice(0,1))
+    return cats.slice(1)
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length-1)
+}
